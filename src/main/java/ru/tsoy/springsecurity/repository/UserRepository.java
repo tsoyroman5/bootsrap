c3ddf12adce914adcdff781e8cd37b1c5ec6be25
tsoyroman <tsoyroman5@gmail.com>
2022-02-10 19:02:05 +0900

    Update

--- src/main/java/ru/tsoy/springsecurity/repository/UserRepository.java
+++ src/main/java/ru/tsoy/springsecurity/repository/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT user FROM User user JOIN FETCH user.roles where user.username = :username")
     User findUserByUsername(@Param("username") String username);
 
-    @Query(value = "SELECT user FROM User user JOIN FETCH user.roles")
+    @Query(value = "SELECT DISTINCT user FROM User user JOIN FETCH user.roles")
     List<User> findAll();
 
     @Query(value = "SELECT user FROM User user JOIN FETCH user.roles where user.id = :id")
